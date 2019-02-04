284c99f59883deb172e07ebc05a3543bde7d20ce
yncxcw <ynjassionchen@gmail.com>
2019-02-04 14:08:45 -0700

    commit experiment

---
+++ experiment/single_thread_sleep.cc
@@ -0,0 +1,47 @@
+/* A single thread app that runs and sleep */
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono> 
+#include <math.h>
+#include <ratio>
+
+using namespace std;
+
+
+int main(){
+
+vector<double> array(1024*10, 40);
+
+long compute, sleep;
+double accumulate=0;
+
+cout<<"input compute time and sleep time in millisecond. "<<endl;
+cin>>compute>>sleep;
+
+//A infinite loop
+while(true){
+
+    if(accumulate > compute && sleep > 0){
+        //sleep for sleep time
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep));
+        accumulate = 0;
+    }else{
+
+        auto begin = std::chrono::high_resolution_clock::now();
+
+        //CPU intensieve computation
+        for(int i=0; i < array.size(); i++)
+            array[i] = sqrt(array[i])*sqrt(i) + 1;
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        accumulate += duration.count() * 1.0 /1000;
+    }
+
+}
+
+return 0;
+
+}
